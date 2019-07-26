5a59b45889cf102c55385e1cee1a577674951f4e
ctong5 <cassandra.tong5@gmail.com>
2019-07-26 11:35:46 -0700

    Complete createWriteStream.write that seeds 10M with drain

--- database2/seed.js
+++ database2/seed.js
@@ -2,72 +2,54 @@ const fs = require('fs');
 const faker = require('faker');
 
 let csvname = 'cookiemonster2.csv'
-let writeStream = fs.createWriteStream(csvname);
+let writer = fs.createWriteStream(csvname);
+let recordcount = 0;
 
-function writeOneMillionTimes(writer, data, encoding, callback) {
-  let i = 100;
+const generateData = () => {
+  const general = faker.lorem.sentences();
+  const thespace = faker.lorem.sentences();
+  const guestaccess = faker.lorem.sentences();
+  const interactionwithguests = faker.lorem.sentences();
+  const otherthings = faker.lorem.sentences();
+  const license = `STR-${faker.random.number({min:1000000, max:9999999})}`;
+  const allLines = `#${recordcount}: ${general}|${thespace}|${guestaccess}|${interactionwithguests}|${otherthings}|${license}\n`;
+  return allLines;
+}
+
+const write10MTimes = () => {
+  let i = 10;
   write();
-
   function write() {
     let ok = true;
-
+    // let allLines = '';
+    // let oneLineDelimitedFinal = '';
+    // const descsubtitle = ['General','The space', 'Guest access', 'Interaction with guests', 'Other things to note','License or registration number'];
+    
     do {
       i--;
       if (i === 0) {
-        //last time!
-        writer.write(data, encoding, callback);
+        let data = generateData();
+        recordcount++;
+        writer.write(data, 'utf8');
       } else {
-        // see if we should continue or wait.
-        // dont pass the callback because we're not done yet.
-        ok = writer.write(data, encoding);
+        // See if we should continue, or wait.
+        // Don't pass the callback, because we're not done yet.
+        let data = generateData();
+        recordcount++;
+        ok = writer.write(data, 'utf8');
       }
-    } 
-
-    while (i > 0 && ok);
+    } while (i > 0 && ok);
     if (i > 0) {
-      // had to stop early!
-      // write some more once it drains.
-      write.once('drain', write);
+      // Had to stop early! Write some more once it drains.
+      writer.once('drain', write);
+    } else if (i === 0) {
+      writer.end();
     }
-    
-  }
-}
+  };
+};
 
-let allLines= '';
-let oneLineFinal = '';
-let recordcount = 0;
+write10MTimes();
 
-// make number of entries
-for (let i = 0; i < 1000000; i++) {
-
-  // create content for each type of description
-  const descsubtitle = ['General','The space', 'Guest access', 'Interaction with guests', 'Other things to note','License or registration number'];
-  let oneLineDelimited = '';
-
-  for (let j = 0; j < descsubtitle.length; j++){
-    if (j === 5) {
-      const licensenum = faker.random.number({min:1000000, max:9999999});
-      const license = `STR-${licensenum}`;
-      oneLineDelimited += `${license}|`;
-    } else {
-      const sentences = faker.lorem.sentences();
-      oneLineDelimited += `${sentences}|`
-    }
-
-    oneLineFinal = 'Record #' + i + ': ' + oneLineDelimited.slice(0, oneLineDelimited.length-1) + '\n';
-  }
-
-  // combine all lines to be inserted
-  allLines += oneLineFinal;
-  recordcount++;
-}
-
-// overwrites what's in the file if the file already exists
-writeStream.write(`${allLines}`);
-
-// finish event is emitted when all data flushed from stream
-writeStream.on('finish', ()  => {
+writer.on('finish', ()  => {
   console.log(`${recordcount} data written to ${csvname}`)
 });
-
-writeStream.end();

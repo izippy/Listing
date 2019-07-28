5a8b74da4b1351c5e7faac7470e08ea600f10636
ctong5 <cassandra.tong5@gmail.com>
2019-07-27 23:59:39 -0700

    Refactor to seed 10M listings, saveable to db. - Add db commands to create table - Clean comments - Correct array presentation from '[]' to '{}', per postgres syntax

--- database2/seed_listing.js
+++ database2/seed_listing.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
 const faker = require('faker');
+const db = require('./index.js');
 
 let csvname = 'listing.csv'
 let writer = fs.createWriteStream(csvname);
 let recordcount = 0;
 
+
+// STEP 1: POSTGRES > connect izippy
+// STEP 2: 'node seed_listing.js' to (1) populate csv and (2) create table listing
+// STEP 3: pv in new terminal to pipe in with progress bar outside of postgres
+  // pv /Users/cassandratong/Documents/gitHub/izippy/listing/Listing/database2/listing.csv | psql -U cassandratong -d izippy -c "COPY listing FROM STDIN DELIMITERS '|';"
+
+// CREATE SCHEMAS/TABLES
+let schemaListing = "DROP TABLE listing; CREATE TABLE IF NOT EXISTS listing (id INT NOT NULL PRIMARY KEY, title TEXT NOT NULL, location TEXT NOT NULL, user_id INT NOT NULL, type INT NOT NULL, bedrmnum INT NOT NULL, bathrmnum INT NOT NULL, guestmax INT NOT NULL, beds TEXT [] NOT NULL, bednum INT NOT NULL, general TEXT NOT NULL, space TEXT NOT NULL, guestaccess TEXT NOT NULL, interactionwithguests TEXT NOT NULL, otherthingstonote TEXT NOT NULL, license VARCHAR(15) NOT NULL);";
+db.query(schemaListing, (err, res) => {
+  if (err) {
+    console.log("err", err);
+  } else {
+    // console.log(res);
+    console.log(`${csvname} table created`);
+  }
+  db.end();
+});
+
+// helper function to create accomodation object
 const generateAccomodation = (type) => {
   let accomodation = {};
   let bedrmnum, bathrmnum;
@@ -21,20 +41,20 @@ const generateAccomodation = (type) => {
       let currBedoption = bedoptions[Math.floor(Math.random() * bedoptions.length)];
       // console.log("YO1 currBedoption SLICED", currBedoption.slice(0,1));
       bedcounter += Number(currBedoption.slice(0,1));
-      beds.push(`'${currBedoption}'`);
+      beds.push(`${currBedoption}`);
     }
-    // console.log("YO1 total:: ",bedcounter);
-    bedsArr = `[${beds}]`
+    // console.log("type1 total:: ",bedcounter);
+    bedsArr = `{${beds}}`
 
   // 'Private room'
   } else if (type === 2) {
     bedrmnum = 1;
     bathrmnum = 1;
     guestmax = faker.random.number({min:1, max:3});
-    beds = `'${bedoptions[Math.floor(Math.random()*bedoptions.length)]}'`;
+    beds = `${bedoptions[Math.floor(Math.random()*bedoptions.length)]}`;
     bedcounter = Number(beds.slice(1,2));
-    // console.log("YO2 total::", bedcounter);
-    bedsArr = `[${beds}]`
+    // console.log("type2 total::", bedcounter);
+    bedsArr = `{${beds}}`
 
   // 'Hotel room'
   } else if (type === 3) {
@@ -43,46 +63,45 @@ const generateAccomodation = (type) => {
     guestmax = faker.random.number({min:bedrmnum, max:bedrmnum*2+2});
     for (let i = 0; i < bedrmnum; i++) {
       let currBedoption = bedoptions[Math.floor(Math.random() * bedoptions.length)];
-      // console.log("YO3 currBedoption SLICED", currBedoption.slice(0,1));
+      // console.log("type3 currBedoption SLICED", currBedoption.slice(0,1));
       bedcounter += Number(currBedoption.slice(0,1));
-      beds.push(`'${currBedoption}'`);
+      beds.push(`${currBedoption}`);
     }
-    // console.log("YO3 total::", bedcounter);
-    bedsArr = `[${beds}]`
+    // console.log("type3 total::", bedcounter);
+    bedsArr = `{${beds}}`
   
   // 'Shared room'
   } else if (type === 4) {   
     bedrmnum = 1;
     bathrmnum = 1;
     guestmax = faker.random.number({min:1, max:bedrmnum*2});
-    beds = `'${bedoptions[Math.floor(Math.random()*bedoptions.length)]}'`;
+    beds = `${bedoptions[Math.floor(Math.random()*bedoptions.length)]}`;
     bedcounter = Number(beds.slice(1,2));
-    // console.log("YO4 total::", bedcounter);
-    bedsArr = `[${beds}]`
+    // console.log("typeO4 total::", bedcounter);
+    bedsArr = `{${beds}}`
   }
   accomodation.bedrmnum = bedrmnum;
   accomodation.bathrmnum = bathrmnum;
   accomodation.guestmax = guestmax;
   accomodation.beds = bedsArr;
   accomodation.bednum = bedcounter;
-  console.log(accomodation);
   return accomodation;
 }
 
 const maketable_listing = (i) => {
   const listingid = i + 1;
   const title = faker.lorem.words();
-  const general = faker.lorem.sentences();
   const location = faker.address.city();
-  const host_id = faker.random.number({min:1, max:i});
+  const user_id = faker.random.number({min:1, max:i});
+  const type = faker.random.number({min:1, max:4}); // 4 types of places, see table unit_type
+  const accomodations = generateAccomodation(type);
+  const general = faker.lorem.sentences();
   const thespace = faker.lorem.sentences();
   const guestaccess = faker.lorem.sentences();
   const interactionwithguests = faker.lorem.sentences();
   const otherthings = faker.lorem.sentences();
   const license = `STR-${faker.random.number({min:1000000, max:9999999})}`;
-  const type = faker.random.number({min:1, max:4}); // 4 types of places, see maketable_unit_type
-  const accomodations = generateAccomodation(type);
-  const allLines = `${listingid}|${title}|${location}|${host_id}|${general}|${thespace}|${guestaccess}|${interactionwithguests}|${otherthings}|${license}|${type}|${accomodations.bedrmnum}|${accomodations.bathrmnum}|${accomodations.guestmax}|${accomodations.beds}|${accomodations.bednum}\n`;
+  const allLines = `${listingid}|${title}|${location}|${user_id}|${type}|${accomodations.bedrmnum}|${accomodations.bathrmnum}|${accomodations.guestmax}|${accomodations.beds}|${accomodations.bednum}|${general}|${thespace}|${guestaccess}|${interactionwithguests}|${otherthings}|${license}\n`;
   return allLines;
 }
 

57b0f43a2ef38d29d542a58df87db90592c263e9
ctong5 <cassandra.tong5@gmail.com>
2019-07-28 23:12:42 -0700

    Create psql table and csv seed

--- database2/seed_amenCategories.js
+++ database2/seed_amenCategories.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
-const faker = require('faker');
+const db = require('./index.js');
 
 let csvname = 'amenitiesCategories.csv'
 let writer = fs.createWriteStream(csvname);
 let grecordcount = 0;
+
+// STEP 1: POSTGRES > connect izippy
+// STEP 2: 'node <seed_file.js>' to (1) populate csv and (2) create table
+// STEP 3: pv in new terminal to pipe in with progress bar outside of postgres
+// pv /Users/cassandratong/Documents/gitHub/izippy/listing/Listing/database2/amenitiesCategories.csv | psql -U cassandratong -d izippy -c "COPY amenitiesCategories FROM STDIN DELIMITERS '|';"
+let schema = "DROP TABLE IF EXISTS amenitiesCategories; CREATE TABLE IF NOT EXISTS amenitiesCategories (id INT NOT NULL PRIMARY KEY, amenCategory TEXT);";
+db.query(schema, (err, res) => {
+  if (err) {
+    console.log("err", err);
+  } else {
+    console.log(`${csvname} table created`);
+  }
+  db.end();
+});
 
 const write1Time = () => {
   write();

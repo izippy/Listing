b03b80424f236911a11859e2847cefac18448fd9
ctong5 <cassandra.tong5@gmail.com>
2019-07-28 20:32:44 -0700

    Seed join table for ListingAmenities

--- database2/seed_joinListingAmenities.js
+++ database2/seed_joinListingAmenities.js
@@ -17,19 +17,14 @@ let glistingcount = 0;
   // pick random categories to represent
     // pick random items to represent
 
-const makejoin_firstlisting_highlight = (joinid, currlistingid) => {
-  const allLines = `${joinid}|${currlistingid}|1\n`; // first highlight for a listing id is always guest/bed/bath count
-  return allLines;
-}
-
-const generateOptionalItems = (joinid, currlistingid, amenityid) => {
+const generateJoinListingAmenity = (joinid, currlistingid, amenityid) => {
   const allLines = `${joinid}|${currlistingid}|${amenityid}\n`; // first highlight for a listing id is always guest/bed/bath count
   return allLines;
 }
 
 const writeMaxTimes = () => {
   let currlistingid = 1; 
-  const lastlistingid = 2; // max is number of listings we need to join. 100 for now
+  const lastlistingid = 1; // max is number of listings we need to join. 100 for now
   const whilemax = lastlistingid + 1;
   let joinid = 1;
   let ok = true;
@@ -47,6 +42,18 @@ const writeMaxTimes = () => {
     10: [46, 47, 48, 49, 50],
   }
 
+  const amenrequiredobj = {
+    'Wifi': [1,10], 
+    'TV': [2,11], 
+    'Iron': [3,12],
+    'Air conditioning': [4,13], 
+    'Laptop friendly workspace': [5,14], 
+    'Heating': [6,15], 
+    'Hot water': [7,16],
+    'Washer': [8,17],
+    'Essentials': [9,18],
+  }
+
   writeJoin();
 
   function writeJoin() {
@@ -59,22 +66,53 @@ const writeMaxTimes = () => {
       // joinid++;
       // glistingcount++;
 
-      // make other (optional) highlights. Add a random number (1, 2, or 3 more) of highlights (from ids 2 to 7)
+      // make required amenities categories (1: basic or 2:notincluded)
+      // loop through amenrequiredobj. slice out either the first or second element in value
+      // to randomly build out cat1 and cat2
+      let cat1basic = [];
+      let cat2notincluded = [];
+      for (let key in amenrequiredobj) {
+        let randomindex = Math.round(Math.random()); // random index from 0 or 1;
+        let basicitem = amenrequiredobj[key].splice(randomindex, 1);
+        cat1basic.push(basicitem);
+      }
+      for (let key in amenrequiredobj) {
+        let arr = amenrequiredobj[key]; // [10]
+        cat2notincluded.push(arr);
+      }
+
+      cat1basic = [].concat(...cat1basic); // [ 10, 2, 12, 13, 14, 6, 7, 17, 9 ]
+      cat2notincluded = [].concat(...cat2notincluded); // [ 1, 11, 3, 4, 5, 15, 16, 8, 18 ]
+      
+      // loop through cat1. get joinid, listingid, and amenityid and plug into helper function
+      for (let i = 0; i < cat1basic.length; i++) {
+        data = generateJoinListingAmenity(joinid, currlistingid, cat1basic[i]);
+        joinid++;
+        glistingcount++;
+        alldata += data;
+      };
+
+      // loop through cat2. do the same as above
+      for (let i = 0; i < cat2notincluded.length; i++) {
+        data = generateJoinListingAmenity(joinid, currlistingid, cat2notincluded[i]);
+        joinid++;
+        glistingcount++;
+        alldata += data;
+      };
+
+      // make optional amenities. Add random optional categories of amenities (from ids 19 to 50)
       let availablecategories = [3, 4, 5, 6, 7, 8, 9, 10];
       let mixedcategories = availablecategories.sort(() => Math.random()-0.5); 
-      let maxmixedcategories = Math.floor(Math.random() * 8) + 1; // random number from 1 to 8... 5
-      let usedcategories = mixedcategories.slice(0, maxmixedcategories); // [2,5,7,6,4]
-      console.log("usedcategoriesARR:", usedcategories);
+      let maxmixedcategories = Math.floor(Math.random() * availablecategories.length) + 1; // random number from 1 to 8
+      let usedcategories = mixedcategories.slice(0, maxmixedcategories);
       
+      // build object of usedcategories and their values of array amenityitems
       let usedcategoriesobj = {};
       for (let i = 0; i < usedcategories.length; i++) {
-        // console.log("usedcategories[i]: ", usedcategories[i]);
         usedcategoriesobj[usedcategories[i]] = amenitiesitemsobj[usedcategories[i]];
       }
-      console.log("usedcategoriesobj", usedcategoriesobj);
 
-      // console.log("usedcategoriesOBJ", usedcategoriesobj);
-      // loop thru usedcategoriesobj, get the items array from each prop, mix it, find the max number you'll select, finalized the items to use
+      // loop thru usedcategoriesobj. get the array amenityitems. mix it, find the max number to select, finalized the random items to use per category
       let availableitems, mixeditems, maxmixeditems;
       let useditems;
       for (let cat in usedcategoriesobj) {
@@ -84,13 +122,13 @@ const writeMaxTimes = () => {
         useditems = mixeditems.slice(0, maxmixeditems); // [3, 1]
 
         for (let i = 0; i < useditems.length; i++) {
-          // save each item number and add to row
-          data = generateOptionalItems(joinid, currlistingid, useditems[i]);
+          data = generateJoinListingAmenity(joinid, currlistingid, useditems[i]);
           joinid++;
           glistingcount++;
           alldata += data;
         }
       }
+
       writer.write(alldata, 'utf8');
       currlistingid++;
 

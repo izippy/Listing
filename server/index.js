336b543ba1fb32ff226af1a4bc76c291091107fe
ctong5 <cassandra.tong5@gmail.com>
2019-08-01 20:06:26 -0700

    Handle POST requests successfully

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ app.get('/listing/desc/:listingID', (req, res) => {
 	const params = [req.params.listingID];
 
 	db.execute(query, params, { prepare: true })
-		.then(data => res.status(200).send(data.rows));
+		.then(data => res.status(200).send(data.rows))
+		.catch(() => res.sendStatus(500));
 	// var id = req.params.listingID
 	// db.findDesc(id,(err,data)=>{
 	//     if(err){
@@ -48,7 +49,8 @@ app.get('/listing/amenity/:listingID',(req,res)=>{
 	const params = [req.params.listingID];
 
 	db.execute(query, params, { prepare: true })
-		.then(data => res.status(200).send(data.rows));
+		.then(data => res.status(200).send(data.rows))
+		.catch(() => res.sendStatus(500));
 
 	// var id = req.params.listingID
 	// db.findAmen(id,(err,data)=>{
@@ -62,7 +64,44 @@ app.get('/listing/amenity/:listingID',(req,res)=>{
 	// 				}
 	// 		}
 	// })
-})
+});
+
+app.post('/listing/desc/:listingID',(req,res)=>{
+	console.log('req.params.listingID: ', req.params)
+	const query = `INSERT into izippy.listings(id, title, loc, username, pic, detailtype, detailbedrmnum, detailbathrmnum, detailguestmax,\ 
+detailbeds, detailbednum, highlight1, highlight2, highlight3, highlight4, general, thespace, guestaccess, interactionwithguests,\ 
+otherthingstonote, license) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+	const params = [10000001, 'Porky pig bacon house', 'Farmville', 'Porky', 'https://s3.amazonaws.com/uifaces/faces/twitter/envex/128.jpg','Entire place', 2, 1, 2, ['1 single', '1 single'], 2, 'h1', 'h2', 'h3', 'h4', 'general', 'space', 'guestaccess', '*', '*', 'STR-12345567'];
+	db.execute(query, params, { prepare: true })
+		.then(() => res.sendStatus(201));
+});
+
+app.post('/listing/amenity/:listingID',(req,res)=>{
+	console.log('req.params.listingID: ', req.params)
+
+	const queries = [
+		{
+			query: `INSERT INTO izippy.amenities (id, categories, item, itemdescription) VALUES (?, ?, ?, ?)`,
+			params: [10000001, 'Basic', 'Wifi', 'Continuous access in the listing']
+		},
+		{
+			query: `INSERT INTO izippy.amenities (id, categories, item, itemdescription) VALUES (?, ?, ?, ?)`,
+			params: [10000001, 'Not included', 'Air conditioning', '*']
+		},
+		{
+			query: `INSERT INTO izippy.amenities (id, categories, item, itemdescription) VALUES (?, ?, ?, ?)`,
+			params: [10000001, 'Not included', 'Heating', '*']
+		},
+		{
+			query: `INSERT INTO izippy.amenities (id, categories, item, itemdescription) VALUES (?, ?, ?, ?)`,
+			params: [10000001, 'Bed_and_bath', 'Shampoo', '*']
+		},
+	];
+
+	db.batch(queries, { prepare: true })
+		.then(() => res.sendStatus(201));
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 module.exports = app;

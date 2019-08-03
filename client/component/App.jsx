9b93fd1fe7bfbe735b9e3e27bbcb0ed47b31b049
ctong5 <cassandra.tong5@gmail.com>
2019-08-03 12:07:16 -0700

    Refactor to handle passed in data

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -5,70 +5,89 @@ import Description from './Description';
 import styles from './style/App.css';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            title: '',
-            location: '',
-            host: {},
-            highlights: {},
-            desc: {},
-            detail: {},
-            amenity: {},
-        }
-    }
+	constructor(props){
+			super(props);
+			this.state = {
+					title: '',
+					location: '',
+					host: {},
+					highlights: {},
+					desc: {},
+					detail: {},
+					amenity: {},
+			}
+	}
 
-    componentDidMount(){
-        this.getDescInfo();
-        this.getAmenInfo();
-    }
+	componentDidMount(){
+			this.getDescInfo();
+			this.getAmenInfo();
+	}
 
-    getDescInfo(){
-        const parts = window.location.href.split('/');
-        const id = parts[parts.length - 2];
-        $.ajax({
-            method:'GET',
-            url: `/listing/desc/${id}`,
-            contentType: 'application/json',
-            success: (desc)=>{
-                console.log(desc)
-                this.setState({
-                    title: desc.title,
-                    location: desc.location,
-                    host: desc.host,
-                    highlights: desc.highlights,
-                    desc: desc.desc,
-                    detail: desc.detail    
-                })
-            }
-        })
-    }
+	getDescInfo(){
+		const parts = window.location.href.split('/');
+		const id = parts[parts.length - 2];
+		$.ajax({
+			method:'GET',
+			url: `/listing/desc/${id}`,
+			contentType: 'application/json',
+			success: (desc)=>{
+				console.log(desc)
+				let host = {
+					host: desc.host,
+					pic: desc.pic,
+				};
 
-    getAmenInfo(){
-        const parts = window.location.href.split('/');
-        const id = parts[parts.length - 2];
-        $.ajax({
-            method:'GET',
-            url: `/listing/amenity/${id}`,
-            contentType: 'application/json',
-            success: (amenity)=>{
-                console.log(amenity)
-                this.setState({
-                    amenity: amenity.amenities
-                })
-            }
-        })
-    }
-    
-    render() {
-        return (
-            <div className={styles.main}>
-                <Description  
-                title={this.state.title} location={this.state.location} host={this.state.host} highlights={this.state.highlights} desc={this.state.desc} detail={this.state.detail} amenity={this.state.amenity}
-                />
-            </div>
-        )
-    }
+				let hprop1 = desc.highlight1.split(':')[0];
+				let hprop2 = desc.highlight2.split(':')[0];
+				let hprop3 = desc.highlight3.split(':')[0];
+				let hprop4 = desc.highlight4.split(':')[0];
+				let highlights = {
+					[hprop1]: desc.highlight1.split(':')[1],
+					[hprop2]: desc.highlight2.split(':')[1],
+					[hprop3]: desc.highlight3.split(':')[1],
+					[hprop4]: desc.highlight4.split(':')[1],
+				}
+
+				this.setState({
+					title: desc.title,
+					location: desc.loc,
+					host: host,
+					highlights: highlights,
+					desc: desc.desc,
+					detail: desc.detail    
+				});
+			},
+				error: (err) => {
+					console.log(err);
+				}
+		})
+	}
+
+	getAmenInfo(){
+		const parts = window.location.href.split('/');
+		const id = parts[parts.length - 2];
+		$.ajax({
+			method:'GET',
+			url: `/listing/amenity/${id}`,
+			contentType: 'application/json',
+			success: (amenity)=>{
+					console.log(amenity)
+					this.setState({
+							amenity: amenity.amenities
+					})
+			}
+		})
+	}
+	
+	render() {
+		return (
+				<div className={styles.main}>
+						<Description  
+						title={this.state.title} location={this.state.location} host={this.state.host} highlights={this.state.highlights} desc={this.state.desc} detail={this.state.detail} amenity={this.state.amenity}
+						/>
+				</div>
+		)
+	}
 }
     
 export default App;

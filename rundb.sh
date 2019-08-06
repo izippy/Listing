5ca38cc520f97fa8e34d77ab388d89dfd5861d50
ctong5 <cassandra.tong5@gmail.com>
2019-08-05 18:07:45 -0700

    Uncomment to create tables in cassandra

--- rundb.sh
+++ rundb.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#createdb, run with sh <this filename>
-# ????psql postgres < ./database2/schema.sql",
 # psql -h localhost -f postgres/postgresql.sql
 # echo "done with postgres"
 
-cqlsh -f database3/schema_listings.cql
-echo "done with cassandra_listings"
+cqlsh -f database3/schema_amenities.cql
+echo "done with cassandra_amenities"
 
-# cqlsh -f database3/schema_amenities.cql
-# echo "done with cassandra_amenities"
+# cqlsh -f database3/schema_listings.cql
+# echo "done with cassandra_listings"
